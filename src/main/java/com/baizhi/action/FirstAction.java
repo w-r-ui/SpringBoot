3294b00bcbc8fa0107b3268008e41667fb8d2f35
w-r-ui <57436664+w-r-ui@users.noreply.github.com>
2019-11-06 18:59:21 +0800

    Update FirstAction.java

--- src/main/java/com/baizhi/action/FirstAction.java
+++ src/main/java/com/baizhi/action/FirstAction.java
@@ -2,7 +2,7 @@ package com.baizhi.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//这是在github上修改过后的
 @Controller
 @RequestMapping("/first")
 public class FirstAction {
